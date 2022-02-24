4db1d6e3f4b7dc1c9679d9fb55516cffffce11e5
Chris Anacan <chrisanacan@gmail.com>
2022-02-24 09:27:42 -0600

    Hospital MVC

---
+++ Hospital MVC/Hospital MVC/Migrations/20220223162322_AddingApplicantsTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hospital_MVC.Migrations
+{
+    public partial class AddingApplicantsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicantsTables",
+                table: "ApplicantsTables");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicantsTables",
+                newName: "ApplicantsTable");
+
+            migrationBuilder.RenameColumn(
+                name: "Doctor_Name",
+                table: "ApplicantsTable",
+                newName: "Applicant_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicantsTable",
+                table: "ApplicantsTable",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicantsTable",
+                table: "ApplicantsTable");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicantsTable",
+                newName: "ApplicantsTables");
+
+            migrationBuilder.RenameColumn(
+                name: "Applicant_Name",
+                table: "ApplicantsTables",
+                newName: "Doctor_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicantsTables",
+                table: "ApplicantsTables",
+                column: "ID");
+        }
+    }
+}
